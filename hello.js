b3cbc529f7088fc4232d180bcfd05eec60a2c51e
Blythe Fernandes <fernandesblythe@gmail.com>
2025-06-05 10:33:57 +0530

    i learned function

--- hello.js
+++ hello.js
@@ -1,6 +1,10 @@
-let even =9;
-if(even %2== 0){
-    console.log("no. is even")}
-else{
-    console.log("no. is odd")
+// const prompt = require('prompt-sync')();
+// const name = prompt("What is your name?");
+// console.log("Hello, "+ name + "!");
+
+function sq (x){
+    let ans =0;
+    ans = x*x;
+    return ans;
 }
+console.log("the answer is:" +sq(2));
